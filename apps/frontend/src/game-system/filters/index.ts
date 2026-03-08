1556af7deee83dd7dd4b267c8b0392e4684162ed
Bogdan Ohorodnyk <ogorodnyk.bogdan@gmail.com>
2026-03-09 01:54:18 +0200

    refactor: remove duplicate filter initialization logic
    - Remove unused game-filter-utils.ts file
    - createInitialGameFilters() and resetGameFilters() were not used
    - initialGameFilters constant is the single source of truth
    - Update barrel export in filters index.ts

--- apps/frontend/src/game-system/filters/index.ts
+++ apps/frontend/src/game-system/filters/index.ts
@@ -1,6 +1,5 @@
 export * from "./game-filter-config";
 export * from "./game-filter-types";
-export * from "./game-filter-utils";
 export * from "./game-filters-sidebar";
 export * from "./game-filters-state";
 export * from "./game-filters";
